e0d7930b6d87ba1849a8e754b20d0e0b21bd30cf
zmbrahian <53061823+zmbrahian@users.noreply.github.com>
2021-07-28 14:55:29 -0500

    Create db_seed.sql

---
+++ lab-1/db_seed.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE mydb;
+USE mydb;
+CREATE TABLE mytable(
+    id INT NOT NULL AUTO_INCREMENT,
+    message VARCHAR(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+INSERT INTO mytable(message) VALUES
+    ('Hello World!'),
+    ('foo'),
+    ('bar')
+;
+GRANT ALL PRIVILEGES ON mydb.* TO 'userdba'@'%';
